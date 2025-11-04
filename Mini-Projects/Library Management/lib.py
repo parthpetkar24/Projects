bb6aa7f498b3446862006443bcdadd70ea36b03b
parthpetkar24 <parth.petkar2020@gmail.com>
2025-11-04 05:43:18 +0530

    Changes

--- Mini-Projects/Library Management/lib.py
+++ Mini-Projects/Library Management/lib.py
@@ -5,6 +5,9 @@ from time import sleep
 def add_book(libFrame):
     book_id=int(input("\nEnter Book Id: "))
     book_name=input("Enter Book Name: ")
+    if ((libFrame['Id'] == book_id) & (libFrame['Books'] == book_name)).any():
+        print("\nThis book already exists in the library!")
+        return  
     book_author=input("Enter Book Author Name: ")
     book_year=input("Enter Book Publication Year: ")
     libFrame.loc[len(libFrame)]={
@@ -12,7 +15,7 @@ def add_book(libFrame):
         "Books":book_name.lower(),
         "Author":book_author.lower(),
         "Year":book_year}
-    libFrame.to_csv('Library Management/index.csv',index=False)
+    libFrame.to_csv('Mini-Projects/Library Management/index.csv',index=False)
     print("\nBook Added Successfully!!")
 
 def issue_book(libFrame,issueFrame):
@@ -31,8 +34,8 @@ def issue_book(libFrame,issueFrame):
                 }
             indice=libFrame[libFrame["Id"]==issue_id].index
             libFrame.drop(indice,inplace=True)
-            libFrame.to_csv('Library Management/index.csv')
-            issueFrame.to_csv('Library Management/issue.csv')
+            libFrame.to_csv('Mini-Projects/Library Management/index.csv',index=False)
+            issueFrame.to_csv('Mini-Projects/Library Management/issue.csv',index=False)
         else:
             print("Book Not Avaliable in Library!")
             break 
@@ -46,7 +49,7 @@ def remove_book(libFrame):
             if id == removebyid:
                 indice=libFrame[libFrame["Id"]==removebyid].index
                 libFrame.drop(indice,inplace=True)
-                libFrame.to_csv('Library Management/index.csv',index=False)
+                libFrame.to_csv('Mini-Projects/Library Management/index.csv',index=False)
                 print("Book Removed Successfully!")
                 found=True
         if not found:
@@ -57,7 +60,7 @@ def remove_book(libFrame):
             if name == removebyname.lower():
                 indice=libFrame[libFrame["Books"]==removebyname].index
                 libFrame.drop(indice,inplace=True)
-                libFrame.to_csv('Library Management/index.csv',index=False)
+                libFrame.to_csv('Mini-Projects/Library Management/index.csv',index=False)
                 print("Book Removed Successfully!")
                 found=True
         if not found:
@@ -66,14 +69,16 @@ def remove_book(libFrame):
         print("Invalid Input!")
 
 def view_current(libFrame):
-    print("Current Library Data: \n")
-    print(libFrame)
+    print(f"Current Library Data: \n{libFrame}")
+
+def view_issued_books(issueFrame):
+    print(f"Issued Books: \n{issueFrame}")
 
 if __name__=="__main__":
     print("Welcome to Library Managment System!\n")
     sleep(1)
-    file_name='Library Management/index.csv'
-    issuer_file='Library Management/issue.csv'
+    file_name='Mini-Projects/Library Management/index.csv'
+    issuer_file='Mini-Projects/Library Management/issue.csv'
     if os.path.isfile(file_name) and os.path.isfile(issuer_file):
         print("Opening File...")
     else:
@@ -92,15 +97,15 @@ if __name__=="__main__":
         }
         libFrame=pd.DataFrame(lib)
         issueFrame=pd.DataFrame(issue)
-        libFrame.to_csv('Library Management/index.csv',index=False)
-        issueFrame.to_csv('Library Management/issue.csv',index=False)
+        libFrame.to_csv('Mini-Projects/Library Management/index.csv',index=False)
+        issueFrame.to_csv('Mini-Projects/Library Management/issue.csv',index=False)
         print("File Created!")
 
-    libFrame=pd.read_csv('Library Management/index.csv')
-    issueFrame=pd.read_csv('Library Management/issue.csv')
+    libFrame=pd.read_csv('Mini-Projects/Library Management/index.csv')
+    issueFrame=pd.read_csv('Mini-Projects/Library Management/issue.csv')
     print("\nLibrary Manager Opened!")
     while True:
-        print("\nOperations Available:\n1.Add Book\t2.Issue Book\t3.Remove book from Library\n4.View Current Books in Library\t5.Exit")
+        print("\nOperations Available:\n1.Add Book\t2.Issue Book\t3.Remove book from Library\n4.View Current Books in Library\t5.View Issued Books\t6.Exit")
         action=int(input("Enter Index of Operation to be performed(1/2/3/4): "))
         match(action):
             case 1:
@@ -112,6 +117,8 @@ if __name__=="__main__":
             case 4:
                 view_current(libFrame)
             case 5:
+                view_issued_books(issueFrame)
+            case 6:
                 print("Thank You for using Library Management System! ")
                 break
             case _:
