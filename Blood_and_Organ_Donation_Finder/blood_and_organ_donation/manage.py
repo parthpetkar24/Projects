3423be11db1acd379d8db030e3e791f5ad56adde
parthpetkar24 <parth.petkar2020@gmail.com>
2025-12-26 12:19:06 +0530

    Backend for Blood and Organ Donation created

--- Blood_and_Organ_Donation_Finder/blood_and_organ_donation/manage.py
+++ Blood_and_Organ_Donation_Finder/blood_and_organ_donation/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blood_and_organ_finder.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blood_and_organ_donation.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- Blood_and_Organ_Donation_Finder/blood_and_organ_finder/blood_and_organ_finder/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-def homepage(request):
-    homepage_file='Front-End/home.html'
-    return HttpResponse(homepage_file)
