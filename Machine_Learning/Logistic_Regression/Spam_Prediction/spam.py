fff8084f7a266048415957c8ef46d0a4ce77084b
parthpetkar24 <parth.petkar2020@gmail.com>
2026-03-08 09:07:11 +0530

    Spam or Not

---
+++ Machine_Learning/Logistic_Regression/Spam_Prediction/spam.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+data=pd.read_csv()
