3eb63d0827535e63b43efc370b9b06344bc5f2c5
parthpetkar24 <parth.petkar2020@gmail.com>
2026-04-13 11:43:46 +0530

    Changes

--- Blood_and_Organ_Donation_Finder/blood_and_organ_donation/hospital_dashboard/urls.py
+++ Blood_and_Organ_Donation_Finder/blood_and_organ_donation/hospital_dashboard/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name="hospital_dashboard"
 
 urlpatterns = [
-    path('hospital_dashboard/',views.hospital_dashboard,name="hospital_dashboard"),
+    path('',views.hospital_dashboard,name="hospital_dashboard"),
     path('approve_application/', views.approve_application, name='approve_application'),
     path('reject_application/', views.reject_application, name='reject_application'),
 ]
