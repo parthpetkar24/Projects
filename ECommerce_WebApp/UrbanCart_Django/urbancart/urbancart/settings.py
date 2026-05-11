95a6a407d6af8624e1985b38f3489cf2c2fb7c19
parthpetkar24 <parth.petkar2020@gmail.com>
2026-05-11 15:11:45 +0530

    Basic Homepage

--- ECommerce_WebApp/UrbanCart_Django/urbancart/urbancart/settings.py
+++ ECommerce_WebApp/UrbanCart_Django/urbancart/urbancart/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'urbancart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[BASE_DIR/"static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
