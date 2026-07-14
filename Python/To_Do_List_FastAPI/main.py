b3efcf0abd01a73b8e5922260517462835c40e6f
parthpetkar24 <parth.petkar2020@gmail.com>
2026-07-14 05:53:03 +0530

    To Do Backend

--- Python/To_Do_List_FastAPI/main.py
+++ Python/To_Do_List_FastAPI/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI,Depends,HTTPException
+from fastapi import FastAPI,Depends,HTTPException,status
 from db import To_Do_Task,create_db_and_tables,get_async_session
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
-from schemas import TaskCreate,TaskShow
+from schemas import TaskCreate,TaskShow,TaskUpdate
 from typing import List
 from uuid import UUID
+from fastapi.responses import Response
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -42,3 +43,27 @@ async def get_task_by_id(task_id:UUID ,session:AsyncSession=Depends(get_async_se
     if tasks is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return tasks
+
+@app.put("/update-task/{task_id}",response_model=TaskShow)
+async def update_task(task_id:UUID,task_in:TaskUpdate,session:AsyncSession=Depends(get_async_session)):
+    result=await session.execute(select(To_Do_Task).where(To_Do_Task.id==task_id))
+    task=result.scalar_one_or_none()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    update_data=task_in.model_dump(exclude_unset=True)
+    for field,value in update_data.items():
+        setattr(task,field,value)
+
+    await session.commit()
+    await session.refresh(task)
+    return task
+
+@app.delete("/delete-task/{task_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(task_id:UUID,session:AsyncSession=Depends(get_async_session)):
+    result=await session.execute(select(To_Do_Task).where(To_Do_Task.id==task_id))
+    task=result.scalar_one_or_none()
+    if task is None:
+        raise HTTPException(status_code=404,detail="Task Not Found")
+    await session.delete(task)
+    await session.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- Python/To_Do_List_FastAPI/schemas.py
+++ Python/To_Do_List_FastAPI/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel,ConfigDict
 from datetime import date, time
 from uuid import UUID
 from db import TaskStatus
+from typing import Optional
 
 class TaskCreate(BaseModel):
     task:str
@@ -16,3 +17,9 @@ class TaskShow(BaseModel):
     due_date: date
     status: TaskStatus
     model_config = ConfigDict(from_attributes=True)
+
+class TaskUpdate(BaseModel):
+    task: Optional[str] = None
+    due_date: Optional[date] = None
+    status: Optional[TaskStatus] = None
+    
